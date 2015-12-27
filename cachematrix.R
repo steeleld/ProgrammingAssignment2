3cbd798265d7261c2d96a57583f506ebc06fc890
steeleld <steele.ld@gmail.com>
2015-12-26 23:24:52 -0400

    Initial changes to cachematrix.R file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Programming Assignment 2
+## Course identifier: rprog-035
+## Overall assignment is to provide a program that can cache
+## time-consuming computations as an example of R lexical scoping.
 
-## Write a short comment describing this function
+## Program will cache the inverse of a matrix. 
+cachematrix <- function() {
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## makeCacheMatrix creates a special "matrix" object than can cache its inverse
+        
+        makeCacheMatrix <- function(x = matrix()) {
+                m <- NULL
+                set <- function(y) {
+                        x <<- y
+                        m <<- NULL
+                }
+                get <- function() x
+                setsolve <- function(solve) m <<- mean
+                getsolve <- function() m
+                list(set = set, get = get,
+                     setsolve = setsolve,
+                     getsolve = getsolve)
+        }
+        
+        ## cacheSolve computes the inverse of the special "matrix" returned by 
+        ## makeCacheMatrix above.  If inverse already calculated (and matrix not
+        ## changed since then), then cacheSolve retrieves the already calculated value.
+        
+        cacheSolve <- function(x, ...) {
+                ## Return a matrix that is the inverse of 'x'
+                m <- x$getsolve()
+                if(!is.null(m)) {
+                        message("getting cached data")
+                        return(m)
+                }
+                data <- x$get()
+                m <- solve(data, ...)
+                x$setsolve(m)
+                m
+        }
 }
